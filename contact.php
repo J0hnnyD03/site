d32fb7d3e1def5b468d7633502f3106cd000695f
J0hnnyD03 <rudinhohess@hotmail.com>
2022-10-17 17:29:48 -0300

    contact

--- contact.php
+++ contact.php
@@ -1,4 +1,39 @@
 <?php
+function contact() {
+  $to = "atenimento@hmaxti.com.br";
+  $subject = "[Contato] Website";
+  $headers = "MIME-Version: 1.0" . "\r\n";
+  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+  $headers .= "From: webmaster@hmaxti.com.br" . "\r\n" . "CC: hess.rudinho@gmail.com";
+
+  foreach ($_POST as $param_name => $param_val) {
+      echo "Param: $param_name; Value: $param_val<br />\n";
+  }
+
+  $message = "
+  <html>
+  <head>
+  <title>Solicitação de Contato via Website</title>
+  </head>
+  <body>
+  <p>This email contains HTML Tags!</p>
+  <table>
+  <tr>
+  <th>Firstname</th>
+  <th>Lastname</th>
+  </tr>
+  <tr>
+  <td>John</td>
+  <td>Doe</td>
+  </tr>
+  </table>
+  </body>
+  </html>
+  ";
+
+  $this_mail = mail($to,$subject,$message,$headers);
+ throw new Error;
+}
 // the message
 // $msg = "First line of text\nSecond line of text";
 
@@ -8,40 +43,11 @@
 // send email
 //mail("someone@example.com","My subject",$msg);
 
-$to = "atenimento@hmaxti.com.br";
-$subject = "[Contato] Website";
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers .= "From: webmaster@hmaxti.com.br" . "\r\n" . "CC: hess.rudinho@gmail.com";
 
-foreach ($_POST as $param_name => $param_val) {
-    echo "Param: $param_name; Value: $param_val<br />\n";
+try {
+  contact();
+} catch(Error $e) {
+ var_dump($e->getTrace());
 }
 
-$message = "
-<html>
-<head>
-<title>Solicitação de Contato via Website</title>
-</head>
-<body>
-<p>This email contains HTML Tags!</p>
-<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-</tr>
-<tr>
-<td>John</td>
-<td>Doe</td>
-</tr>
-</table>
-</body>
-</html>
-";
-
-$this_mail = mail($to,$subject,$message,$headers);
-
-if (!$this_mail) {
-    echo error_get_last()['message'];
-}
 ?>
